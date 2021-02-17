5ee88f89c233fcf40d12222a491ef523eae31f85
Kas-Miri <katya.sharfina00@gmail.com>
2021-02-17 15:38:11 +0300

    base

---
+++ models/index.js
@@ -0,0 +1,8 @@
+exports.Identity = {
+  ID: 1
+  };
+  
+  exports.Status = require("./status").Status;
+  exports.Task = require("./task").Task;
+  exports.MainDTO = require("./mainDTO").MainDTO;
+  
---
+++ models/mainDTO.js
@@ -0,0 +1,9 @@
+exports.MainDTO = class {
+    constructor(title, tasks, statuses, editItem){
+    this.title = title || '';
+    this.tasks = tasks || [];
+    this.statuses = statuses || [];
+    this.editItem = editItem || null;
+    }
+}
+    
